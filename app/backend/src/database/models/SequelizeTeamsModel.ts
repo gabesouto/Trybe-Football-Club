d3ea478067879ca8da995d36447cd3bfdfcb840e
gabesouto <soutogabriel04@gmail.com>
2023-07-13 17:29:53 -0300

    Ajustes dockerfile/ model table Teams

--- app/backend/src/database/models/SequelizeTeamsModel.ts
+++ app/backend/src/database/models/SequelizeTeamsModel.ts
@@ -1,37 +1,37 @@
-// // src/database/models/SequelizeBook
-// import {
-//   DataTypes,
-//   Model,
-//   InferAttributes,
-//   InferCreationAttributes,
-//   CreationOptional,
-// } from 'sequelize';
-// import db from '.'
+// src/database/models/SequelizeBook
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
 
-// class SequelizeTeams extends Model<InferAttributes<SequelizeTeams>,
-// InferCreationAttributes<SequelizeTeams>> {
-//   declare id: CreationOptional<number>;
+class SequelizeTeams extends Model<InferAttributes<SequelizeTeams>,
+InferCreationAttributes<SequelizeTeams>> {
+  declare id: CreationOptional<number>;
 
-//   declare teamName: string;
-// }
+  declare teamName: string;
+}
 
-// SequelizeTeams.init({
-//   id: {
-//     type: DataTypes.INTEGER,
-//     allowNull: false,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   teamName: {
-//     type: DataTypes.STRING(30),
-//     allowNull: false,
-//   },
+SequelizeTeams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  },
 
-// }, {
-//   sequelize: db,
-//   modelName: 'teams',
-//   timestamps: false,
-//   underscored: true,
-// });
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
 
-// export default SequelizeTeams;
+export default SequelizeTeams;
