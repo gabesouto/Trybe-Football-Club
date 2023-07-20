f977f4356b85308bd4e7d42852e4350adc36d394
gabesouto <soutogabriel04@gmail.com>
2023-07-20 14:54:05 -0300

    GET method /matches / funcionalidade ok

--- app/backend/src/models/MatchModel.ts
+++ app/backend/src/models/MatchModel.ts
@@ -1,3 +1,4 @@
+import SequelizeTeams from '../database/models/SequelizeTeamsModel';
 import SequelizeMatches from '../database/models/SequelizeMatchesModel';
 import { IMatch, IMatchModel } from '../Interfaces';
 
@@ -5,7 +6,13 @@ export default class MatchModel implements IMatchModel {
   private model = SequelizeMatches;
 
   public async findAll(): Promise<IMatch[]> {
-    const allMatches = await this.model.findAll();
+    const allMatches = await this.model.findAll({
+      attributes: { exclude: ['home_team_id', 'away_team_id'] },
+      include: [
+        { model: SequelizeTeams, association: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: SequelizeTeams, association: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+    });
 
     return allMatches;
   }
